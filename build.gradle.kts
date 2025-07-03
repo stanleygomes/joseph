d12cd0a7942b7d96df3dadbe706124f7a38c4f82
stanley <stanleygomesdasilva@gmail.com>
2025-07-03 08:14:17 -0300

    chore: add database dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,10 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
