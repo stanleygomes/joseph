5976f38083243c3e9194520f9302c88bcaa8febc
stanley <stanleygomesdasilva@gmail.com>
2025-07-03 06:05:29 -0300

    feat: initialize app with spring and kotlin and gradle

---
+++ src/main/kotlin/com/nazarethlabs/joseph/JosephApplication.kt
@@ -0,0 +1,11 @@
+package com.nazarethlabs.joseph
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JosephApplication
+
+fun main(args: Array<String>) {
+	runApplication<JosephApplication>(*args)
+}
