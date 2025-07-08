fcf008e589a75fd4b60f29a70eb6fcde5af25aa4
stanley <stanleygomesdasilva@gmail.com>
2025-07-08 06:41:24 -0300

    chore: ajustes na config de release.yml [show chore e outros]

---
+++ .version-changelog.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  types: [
+    { type: 'feat', section: 'Features' },
+    { type: 'fix', section: 'Bug Fixes' },
+    { type: 'chore', section: 'Chores', hidden: false },
+    { type: 'docs', section: 'Documentation', hidden: false },
+    { type: 'test', section: 'Tests', hidden: false },
+    { type: 'refactor', section: 'Refactoring', hidden: false },
+    { type: 'style', section: 'Style', hidden: false },
+    { type: 'perf', section: 'Performance', hidden: false },
+    { type: 'ci', section: 'CI', hidden: false },
+    { type: 'build', section: 'Build', hidden: false }
+  ]
+};
+
